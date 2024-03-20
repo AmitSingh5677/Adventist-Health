582b8cbb6d30c0a5783fcfbeac202bff666f3053
Chauhan Amit <chauhan.amit@dgsl.in>
2024-03-20 15:40:30 +0530

    verification & connect bank

--- src/businessPages/pages/OrderHistory/index.js
+++ src/businessPages/pages/OrderHistory/index.js
@@ -1,3 +1,4 @@
+import React, { useState,useEffect } from 'react';
 import AppHeader from '../../components/AppHeader/AppHeader'
 import DashboardFooter from '../../components/DashboardFooter/DashboardFooter'
 import AppFooter from '../../components/AppFooter/AppFooter'
@@ -5,6 +6,27 @@ import './index.css'
 
 
 const OrderHistoryBusiness = ()=>{
+  const [orders,setOrders] = useState([])
+
+  const token = JSON.parse(sessionStorage.getItem("token"))
+  // need to work on this
+  const id = JSON.parse(sessionStorage.getItem("userId"))
+
+  const fetch_order_history = async()=>{
+    const response = await fetch(`https://dmecart-38297.botics.co/business/dashboard/${id}/`, {
+      method: 'GET',
+      headers: {
+          'Authorization': `Token ${token}`
+      }})
+      const resData = await response.json()
+      setOrders(resData)
+  }
+  console.log(orders,"hello")
+
+  useEffect(()=>{
+  fetch_order_history()
+  },[])
+
     return <div>
       <div style={{display:'block'}}>
         <AppHeader />
@@ -29,6 +51,17 @@ const OrderHistoryBusiness = ()=>{
     </thead>
 
     <tbody>
+      {orders?.map((item)=>{
+        return(
+          <tr className='border-bottom border-body-secondary '>
+          <td className='py-2'>{item.equipment_name}</td>
+          <td>{item.date}</td>
+          <td className='text-wrap'> {item.payment_status}</td>
+          <td>{item.order_details}</td>
+          <td className='text-success'>{item.status}</td>
+        </tr>
+        )
+      })}
     <tr className='border-bottom border-body-secondary '>
         <td className='py-2'>WHEEL CHAIR</td>
         <td>01/24/2024</td>
